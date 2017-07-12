441c135dd8f68a58e510468abcfce8242f06ce27
Ben Manning <ben@colus.com>
2017-07-12 03:33:06 +0100

    Add alert for failed token

--- app/src/main/java/com/fireflylearning/tasksummary/LoginActivity.java
+++ app/src/main/java/com/fireflylearning/tasksummary/LoginActivity.java
@@ -32,6 +32,10 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.content.Context;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.Response.Listener;
@@ -44,30 +48,17 @@ import static android.Manifest.permission.READ_CONTACTS;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
-
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
+public class LoginActivity extends AppCompatActivity {
 
     // UI references.
     private EditText mHostView;
     private EditText mTokenView;
     private View mProgressView;
     private View mLoginFormView;
+
+    enum tokenError {
+        networkError, hostError, invalidToken
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,9 +98,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * errors are presented and no actual login attempt is made.
      */
     private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
 
         // Reset errors.
         mHostView.setError(null);
@@ -144,7 +132,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            checkToken();
+            checkToken(host, token);
         }
     }
 
@@ -184,117 +172,70 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
     }
 
-    private void checkToken() {
+    private void checkToken(String host, String token) {
+
+        FireflyRequestQueue.initialise(getApplicationContext(), host);
+
         FireflyRequestQueue requestQueue = FireflyRequestQueue.getInstance();
 
-        StringRequest request = new StringRequest(Request.Method.GET, requestQueue.buildUrl("login/api/checktoken"), new Listener<String>() {
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                requestQueue.buildUrl("login/api/checktoken?ffauth_device_id=123&ffauth_secret=" + token),
+                new Listener<String>() {
             @Override
             public void onResponse(String response) {
-                String res = response;
+
+                showProgress(false);
+                if(!response.equals("OK")) {
+                    showTokenError(tokenError.hostError);
+                }
             }
-        }, new ErrorListener() {
+        },
+                new ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                int code = error.networkResponse.statusCode;
+
+                showProgress(false);
+                showTokenError(error.networkResponse.statusCode == 401 ? tokenError.invalidToken : tokenError.hostError);
             }
         });
 
         requestQueue.addToRequestQueue(request);
     }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-    }
-
-
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
-
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
+    private void showTokenError(tokenError tokenError) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            dialog = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            dialog = new AlertDialog.Builder(this);
         }
+        dialog.setCancelable(true);
+        dialog.setTitle(R.string.error);
 
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
+        String message;
+        switch (tokenError) {
+            case networkError:
+                message = getString(R.string.error_internet_connection);
+                break;
+            case hostError:
+                message = getString(R.string.error_invalid_host);
+                break;
+            case invalidToken:
+                message = getString(R.string.error_invalid_token);
+                break;
+            default:
+                message = "";
+        }
+        dialog.setMessage(message);
 
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
+        dialog.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
             }
+        });
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-            } else {
-                mTokenView.setError(getString(R.string.error_incorrect_token));
-                mTokenView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
+        final AlertDialog alert = dialog.create();
+        alert.show();
     }
 }
 
---
+++ app/src/main/java/com/fireflylearning/tasksummary/TaskSummary.java
@@ -0,0 +1,14 @@
+package com.fireflylearning.tasksummary;
+
+import android.app.Application;
+
+/**
+ * Created by benmanning on 12/07/2017.
+ */
+
+public class TaskSummary extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
